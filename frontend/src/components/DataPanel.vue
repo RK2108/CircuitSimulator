5efa141ac96b01597af2c0e349d7a034b8dd62ee
Rajmond <rajmond.kurti@gmail.com>
2025-11-26 15:44:43 +0000

    Updated Data Panel to fetch from backend

--- frontend/src/components/DataPanel.vue
+++ frontend/src/components/DataPanel.vue
@@ -10,62 +10,67 @@
 </template>
 
 <script setup>
-    // import { toRaw, ref } from 'vue';
-    // import { circuit } from '@/circuit';
+    import { toRaw, ref } from 'vue';
+    import { circuit } from '@/circuit';
 
-    // const result = ref(null);
+    const result = ref(null);
 
-    // async function simulateCircuit(){
-    //     result.value = null;
-    //     try {
-    //         const RawCircuit = toRaw(circuit);
+    async function simulateCircuit(){
+        result.value = null;
+        try {
+            const RawCircuit = toRaw(circuit);
             
-    //         const FormattedComponents = RawCircuit.components?.map((c) => {
-    //             const base = {id: c.id, type: c.componentType, x: c.x, y: c.y };
-    //             if (c.type === 'Resistor'){
-    //                 base.resistance = c.resistance;
-    //             }
-    //             else if (c.type === 'Battery'){
-    //                 base.voltage = c.voltage;
-    //             }
-    //             else if (c.type === 'Lamp'){
-    //                 base.power = c.power;
-    //             }
+            const FormattedComponents = RawCircuit.components?.map((c) => {
+
+                const component = {
+                    id: c.id, 
+                    type: c.componentType,
+                    resistance: c.resistance,
+                    voltage: c.voltage,
+                    power: c.power, 
+                    x: c.x, 
+                    y: c.y 
+                };
                 
-    //             return base;
-    //         }) ?? [];
+                return component;
 
-    //         const FormattedWires = RawCircuit.wires?.map((w) => ({
-    //             StartId: w.startId,
-    //             EndId: w.endId,
-    //         })) ?? [];
+            }) ?? [];
 
-    //         const FormattedCircuit = {
-    //             CircuitId: RawCircuit.CircuitId,
-    //             name: RawCircuit.name ?? 'Untitled Circuit',
-    //             components: FormattedComponents,
-    //             wires: FormattedWires,
-    //         };
+            const FormattedWires = RawCircuit.wires?.map((w) => ({
 
-    //         const response = await fetch(
-    //             'http://localhost:5014/api/circuit/simulate', 
-    //             {
-    //                 method: 'POST',
-    //                 headers: { 'Content-Type': 'application/json' },
-    //                 body: JSON.stringify(FormattedCircuit),
-    //             },
-    //         );
+                StartId: w.startId,
+                EndId: w.endId,
 
-    //         if (!response.ok){
-    //             const errorMessage = await response.text();
-    //             alert(errorMessage);
-    //             return;
-    //         }
-    //     }
-    //     catch (err){
-    //         alert("Something went wrong");
-    //     }
-    // }
+            })) ?? [];
+
+            const name = window.prompt("Enter name");
+
+            const FormattedCircuit = {
+                CircuitId: circuit.circuitId,
+                name: name ?? 'Untitled Circuit',
+                components: FormattedComponents,
+                wires: FormattedWires,
+            };
+
+            const response = await fetch(
+                'http://localhost:5107/api/circuit/simulate', 
+                {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(FormattedCircuit),
+                },
+            );
+
+            if (!response.ok){
+                const errorMessage = await response.text();
+                alert(errorMessage);
+                return;
+            }
+        }
+        catch (err){
+            alert(err);
+        }
+    }
 </script>
 
 <style scoped>
