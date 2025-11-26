3c4d7812ffddec8c9ac03388433b96f64faaf36d
Rajmond <rajmond.kurti@gmail.com>
2025-11-26 14:21:03 +0000

    Added Method to Convert from DTO

--- backend/controllers/CircuitController.cs
+++ backend/controllers/CircuitController.cs
@@ -4,5 +4,62 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api/[controller]")]
 public class CircuitController : ControllerBase
 {
-    
+    [HttpPost("simulate")]
+    public IActionResult Simulate([FromBody] CircuitDTO circuitDto)
+    {
+        Circuit circuit = ConvertFromDTO(circuitDto);
+
+        return Ok(circuit.Name);
+    }
+
+    public Circuit ConvertFromDTO(CircuitDTO circuitDTO)
+    {
+        Circuit circuit = new Circuit(circuitDTO.CircuitId, circuitDTO.Name);
+
+        foreach (var compDTO in circuitDTO.Components)
+        {
+            Component comp = null;
+
+            if (compDTO.type == "Resistor")
+            {
+                comp = new Resistor(compDTO.id, compDTO.resistance, compDTO.x, compDTO.y);
+            }
+            else if (compDTO.type == "Battery")
+            {
+                comp = new Battery(compDTO.id, compDTO.voltage, compDTO.x, compDTO.y);
+            }
+            else if (compDTO.type == "Lamp")
+            {
+                comp = new Lamp(compDTO.id, compDTO.power, compDTO.x, compDTO.y);
+            }
+
+            if (comp != null)
+            {
+                circuit.AddComponent(comp);
+            }
+        }
+
+        foreach (var wireDTO in circuitDTO.Wires)
+        {
+            int start = wireDTO.StartId;
+            int end = wireDTO.EndId;
+
+            if (start == end)
+            {
+                continue;
+            }
+
+            bool startExists = circuit.Components.Any(c => c.Id == start);
+            bool endExists = circuit.Components.Any(c => c.Id == end);
+
+            if (startExists && endExists)
+            {
+                
+                Wire w = new Wire(wireDTO.StartId, wireDTO.EndId);
+                circuit.AddWire(w);
+            }
+        }
+
+        return circuit;
+    }
 }
