62f1164ee5d8d990f74664736c46b676be43a285
Rajmond <rajmond.kurti@gmail.com>
2025-11-26 11:23:58 +0000

    Modified Program.cs file

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // Frontend Port
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+app.UseCors("AllowFrontend" );
+
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
