c2edc62ec62616a1ddf772f96def333c81eb6f35
Rajmond <rajmond.kurti@gmail.com>
2025-11-26 11:33:47 +0000

    Add Component Pallete

---
+++ frontend/src/components/Pallete.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="pallete">
+        <h3>Tools</h3>
+        <div class="options">
+            <label>Battery</label>
+            <input type="radio" value="Battery" v-model="selected"></input>
+            <label>Resistor</label>
+            <input type="radio" value="Resistor" v-model="selected"></input>
+            <label>Lamp</label>
+            <input type="radio" value="Lamp" v-model="selected"></input>
+            <label>Wire</label>
+            <input type="radio" value="Wire" v-model="selected"></input>
+            <label>Delete</label>
+            <input type="radio" value="Delete" v-model="selected"></input>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const selected = ref(null);
+
+    watch(selected, (newVal) => {
+        if (newVal){
+            emit('select', newVal);
+        }
+    })
+
+    const emit = defineEmits(['select']);
+</script>
+
+<style scoped>
+</style>
