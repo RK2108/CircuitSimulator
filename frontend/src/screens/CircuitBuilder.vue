1de83cdeb242a5a4101c44cb5c627408c77d9018
Rajmond <rajmond.kurti@gmail.com>
2025-11-26 11:59:19 +0000

    Added Circuit Modelling Screen

---
+++ frontend/src/screens/CircuitBuilder.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <Pallete @select="tool = $event"/>
+        <Canvas ref="canvas"></Canvas>
+        <DataPanel/>
+    </div>
+</template>
+
+<script setup>
+    import Pallete from '@/components/Pallete.vue';
+    import Canvas from '@/components/Canvas.vue';
+    import DataPanel from '@/components/DataPanel.vue';
+    import { ref, watch } from 'vue';
+import Pallete from '@/components/Pallete.vue';
+import DataPanel from '@/components/DataPanel.vue';
+
+    const tool = ref(null);
+    const canvas = ref(null);
+
+    watch (tool, (newVal) => {
+        if (canvas.value){
+            canvas.value.selectedTool = newVal;
+        }
+    });
+
+</script>
+
+<style scoped>
+</style>
