e013ae6943f8c35bbe8b39cde0daa1f40071a178
Rajmond <rajmond.kurti@gmail.com>
2025-11-26 11:36:45 +0000

    Added Circuit Canvas

---
+++ frontend/src/components/Canvas.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <svg class="canvas">
+            
+        </svg>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    selectedTool = ref(null);
+</script>
+
+<style scoped>
+</style>
