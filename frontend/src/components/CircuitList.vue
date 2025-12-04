2903a4cd2738105324ae9f0351b9068ca398685b
Rajmond <rajmond.kurti@gmail.com>
2025-12-04 11:01:54 +0000

    Added screen & component for selecting a circuit

---
+++ frontend/src/components/CircuitList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-for="circuit in result">
+        <p>Circuit Id: {{ circuit.circuitId }}</p>
+        <p>Name: {{ circuit.name }}</p>
+    </div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue';
+
+const result = ref(null);
+
+onMounted(async () => {
+    const response = await fetch('http://localhost:5107/api/circuit/GetAllCircuits');
+    result.value = await response.json();
+});
+
+</script>
+
+<style scoped></style>
---
+++ frontend/src/screens/CircuitSelector.vue
@@ -0,0 +1,36 @@
+<template>
+    <button>New Circuit</button>
+    <CircuitList/>
+</template>
+
+<script setup>
+    import CircuitList from '@/components/CircuitList.vue';
+
+    async function NewCircuit(){
+        const Id = window.prompt("Enter Circuit Id");
+        const Name = window.prompt("Enter Circuit Name");
+
+        const CircuitInfo = {
+            circuitId: Id,
+            name: Name
+        }
+        
+        try {
+            const response = await fetch(
+                'http://localhost:5107/api/circuit/create', 
+                {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(CircuitInfo),
+                },
+            );
+
+            alert(await response.json());
+        }
+        catch(err){
+            alert(err);
+        }
+    }
+</script>
+
+<style scoped></style>
