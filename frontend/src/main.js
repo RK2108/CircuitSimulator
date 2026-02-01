639d6e8993c0b3d7b44b1d2707524c7f1756185d
Rajmond <rajmond.kurti@gmail.com>
2026-02-01 18:39:21 +0000

    Installed Vuetify & configured

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,26 @@ import { createApp } from 'vue'
 import { router } from './router/router'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'light',
+        themes: {
+            light: {
+                colors: {
+                    primary: '#3b82f6',
+                    secondary: '#6366f1'
+                },
+            },
+        },
+    },
+})
+
+createApp(App).use(router).use(vuetify).mount('#app')
